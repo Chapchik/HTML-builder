2142be1f9d651f8c0f4ee18ea3eb4f1d3ea5cfc2
Dmytro Chernukha <chapchik@icloud.com>
2022-05-24 11:24:55 +0300

    fix: change variable name

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const rs = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
 
 let data = '';
 
-rs.on('data', chunk => data += chunk);
-rs.on('end', () => console.log(data));
-rs.on('error', error => console.log('Error', error.message));
+readableStream.on('data', (chunk) => {
+  data += chunk;
+});
+readableStream.on('end', () => console.log(data));
+readableStream.on('error', (error) => console.log(error.message));
