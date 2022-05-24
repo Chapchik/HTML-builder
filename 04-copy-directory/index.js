79deedb21291e810605f69a9bb87e8a102c1f55c
Dmytro Chernukha <chapchik@icloud.com>
2022-05-24 14:00:48 +0300

    feat: change main function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,26 +3,22 @@ const path = require('path');
 const pathToMainFolder = path.join(__dirname, 'files');
 const pathToCopyFolder = path.join(__dirname, 'files-copy');
 
-( async () => {
-  try {
-    await fs.rm(pathToCopyFolder, { recursive: true, force: true });
-    await CopyDir(pathToMainFolder, pathToCopyFolder);
-    console.log('All is copied! :)');
-  } catch (err) {
-    console.log(err.message);
-  }
+(async function () {
+  await fs.rm(pathToCopyFolder, { recursive: true, force: true });
+  copyDirectory(pathToMainFolder, pathToCopyFolder);
 })();
 
-async function CopyDir(src, dest) {
+async function copyDirectory(src, dest) {
   await fs.mkdir(dest);
   const files = await fs.readdir(src, { withFileTypes: true });
-  for (const file of files) {
-    const currPathToSrc = path.join(src, file.name);
-    const currPathToDest = path.join(dest, file.name);
+
+  for (let file of files) {
+    const pathToMainFile = path.join(src, file.name);
+    const pathToCopyFile = path.join(dest, file.name);
     if (file.isFile()) {
-      await fs.copyFile(currPathToSrc, currPathToDest);
+      await fs.copyFile(pathToMainFile, pathToCopyFile);
     } else {
-      await CopyDir(currPathToSrc, currPathToDest);
+      await copyDirectory(pathToMainFile, pathToCopyFile);
     }
   }
 }
